5de1f5e7bf860646f5bfd2952ff70df4de0b60f5
Calafange <jotave369@gmail.com>
2023-06-06 00:11:17 -0300

    modificação no readme e no ambiente de teste

---
+++ build/CMakeFiles/testes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testes.dir/src/playlist.cpp.o"
+  "CMakeFiles/testes.dir/src/sistema.cpp.o"
+  "CMakeFiles/testes.dir/src/teste.cpp.o"
+  "testes"
+  "testes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
